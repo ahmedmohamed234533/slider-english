09d285e5b42a3519402fea06f24fb282b432284e
ahmedmohamed234533 <ahmdmhmdbdalhlym514@gmail.com>
2024-09-30 15:59:57 +0300

    Add files via upload

---
+++ slider english/Untitled-1.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+});
